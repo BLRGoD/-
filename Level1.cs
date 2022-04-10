dec7c9568c7629beb3b38432d6f09f8e952ef82f
Shuning <ya.aif2002@yandex.ru>
2022-04-10 23:13:22 +0300

    Add project files.

---
+++ Level1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Лабиринт
+{
+    public partial class Level1 : Form
+    {
+        public Level1()
+        {
+            InitializeComponent();
+        }
+
+        private void Level1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)
+            {
+                DialogResult result = (MessageBox.Show("Вы действительно хотите закрыть игру?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2));
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                    this.Close();
+            }
+        }
+
+        void startGame()
+        {
+            Point point;
+            point = labelStart.Location;
+            point.Offset(labelStart.Width / 2, labelStart.Height / 2);
+            Cursor.Position = PointToScreen(point);
+            label6.Visible = true; label7.Visible = true;
+            label9.Visible = true; label8.Visible = true;
+        }
+
+        private void Level1_Shown(object sender, EventArgs e)
+        {
+            startGame();
+        }
+
+        private void woods_MouseEnter(object sender, EventArgs e) // заход на стену
+        {
+            Sound.playFail();
+            DialogResult result = (MessageBox.Show("Вы проиграли, попробуем ещё раз?", "Сообщение", MessageBoxButtons.YesNo,MessageBoxIcon.Question, MessageBoxDefaultButton.Button1));
+            if (result == System.Windows.Forms.DialogResult.Yes)
+                startGame();
+            else
+                DialogResult = System.Windows.Forms.DialogResult.Abort;
+        }
+
+        private void labelEnd_MouseEnter(object sender, EventArgs e)
+        {
+            if (countCubs == 0)
+                DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private int countCubs = 4;
+
+        private void takeCub(object sender, EventArgs e) // поднятие куба
+        {
+            Sound.playTakeMoney();
+            (sender as Label).Visible = false;
+            countCubs--;
+        }
+    }
+}
---
+++ Sound.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+
+namespace Лабиринт
+{
+    static class Sound
+    {
+        static bool isHaveSounds = true;
+        static SoundPlayer soundWin = new SoundPlayer(Properties.Resources.SoundWin);
+        static SoundPlayer soundFail = new SoundPlayer(Properties.Resources.SoundFail);
+        static SoundPlayer soundStart = new SoundPlayer(Properties.Resources.SoundStart);
+        static SoundPlayer soundTakeMoney = new SoundPlayer(Properties.Resources.TakeMoney);
+
+        public static void enableSounds()
+        {
+            isHaveSounds = true;
+        }
+
+        public static void disableSounds()
+        {
+            isHaveSounds = false;
+        }
+
+        public static void playWin()
+        {
+            if(!isHaveSounds)
+                return;
+            soundWin.Play();
+        }
+
+        public static void playFail()
+        {
+            if (!isHaveSounds)
+                return;
+            soundFail.Play();
+        }
+
+        public static void playStart()
+        {
+            if (!isHaveSounds)
+                return;
+            soundStart.Play();
+        }
+
+        public static void playTakeMoney()
+        {
+            if (!isHaveSounds)
+                return;
+            soundTakeMoney.Play();
+        }
+    }
+}
